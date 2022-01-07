fd7cea847f913157bade242ef90c1b235a3c3865
Poongeta <Poonpoonpoonny@gmail.com>
2022-01-07 23:46:55 +0700

    lastedit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quizzler());
@@ -9,6 +11,10 @@ class Quizzler extends StatelessWidget {
       home: Scaffold(
         //TODO: 6.ปรับปรุง UI ตามชอบ
         backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Knowledge Quiz'),
+          backgroundColor: Colors.deepPurple[600],
+        ),
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -29,25 +35,23 @@ class _QuizPageState extends State<QuizPage> {
   //create an empty list
   //TODO: 2.ปรับปรุงให้ scoreKeeper ให้เริ่มต้นเป็นลิสต์ว่าง
   List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+
   ];
 
   //TODO: 4.ปรับปรุงคำถามและคำตอบที่สอดคล้องกัน โดยให้มีคำถาม-คำตอบอย่างน้อย 5 ข้อ อาจเป็นคำถามใหม่ทั้งหมดก็ได้
   List<String> questions = [
-    '1 + 1 = 2',
-    'แดง+น้ำเงิน = ม่วง',
+    'หินบะซอลต์เป็นหินอัคนี',
+    'Fromage ในภาษาฝรั่งเศส แปลว่า ปลา ',
     'คาร์บอนเป็นธาตุอโลหะ',
+    '5! = 360',
+    'Mitochondria is the power house of the cell',
+    'End Of The Quiz, Press Any Buttons to Try Again.'
   ];
-  List<bool> answers = [true, true, true];
+  List<bool> answers = [true, false, true, false, true, true];
 
   int questionNumber = 0;
+
+  int Score = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +61,25 @@ class _QuizPageState extends State<QuizPage> {
       children: <Widget>[
         //TODO: 5.แสดงผลคะแนนที่ได้ กำหนดให้ตอบถูกต้องในแต่ละข้อจะได้ 1 คะแนน (นักเรียนอาจต้องเขียนโปรแกรมในส่วนอื่นด้วย เพื่อสามารถแสดงผลคะแนนที่ถูกต้อง)
 
+
         Expanded(
-          flex: 5,
+          flex: 2,
+          child: Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Center(
+              child: Text(
+                '$Score',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 100.0,
+                  color: Colors.teal,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
@@ -87,6 +108,7 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                 ),
               ),
+
               onPressed: () {
                 bool correctAnswer = answers[questionNumber];
                 if (correctAnswer==true) {
@@ -100,9 +122,41 @@ class _QuizPageState extends State<QuizPage> {
                     //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
                     if (questionNumber < questions.length-1) {
                       questionNumber++;
+                      Score++;
+
                     }
                     else {
                       //TODO: 1.ถ้าคำถามหมดแล้ว ให้เริ่มต้นใหม่ โดยกลับไปที่คำถามเดิมและเคลียร์ scoreKeeper ด้วย
+
+                      questionNumber = 0;
+                      scoreKeeper.clear();
+
+                      Score = 0;
+
+
+                    }
+
+                  });
+                }
+                else{
+                  setState(() {
+                    //เมื่อกดปุ่ม True เพิ่มข้อมูลเข้าไปในลิสต์ scoreKeeper โดยใช้ add method
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+                    //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
+                    if (questionNumber < questions.length-1) {
+                      questionNumber++;
+                    }
+                    else {
+                      //TODO: 1.ถ้าคำถามหมดแล้ว ให้เริ่มต้นใหม่ โดยกลับไปที่คำถามเดิมและเคลียร์ scoreKeeper ด้วย
+
+                      questionNumber = 0;
+                      scoreKeeper.clear();
+
+                      Score = 0;
 
                     }
 
@@ -130,7 +184,87 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //TODO: 3.ปรับปรุงโค้ด เมื่อกดปุ่ม False แล้วให้มีการทำงานในลักษณะเดียวกับปุ่ม True
 
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer==false) {
+                  setState(() {
+                    //เมื่อกดปุ่ม True เพิ่มข้อมูลเข้าไปในลิสต์ scoreKeeper โดยใช้ add method
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ));
+                    //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
+                    if (questionNumber < questions.length-1) {
+                      questionNumber++;
+                      Score++;
+                    }
+                    else {
+                      //TODO: 1.ถ้าคำถามหมดแล้ว ให้เริ่มต้นใหม่ โดยกลับไปที่คำถามเดิมและเคลียร์ scoreKeeper ด้วย
+
+                      questionNumber = 0;
+                      scoreKeeper.clear();
+
+                      Score = 0;
+
+                    }
+
+                  });
+                }
+                else{
+                  setState(() {
+                    //เมื่อกดปุ่ม True เพิ่มข้อมูลเข้าไปในลิสต์ scoreKeeper โดยใช้ add method
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+                    //ตรวจสอบว่าข้อคำถามจะไม่เกิน index
+                    if (questionNumber < questions.length-1) {
+                      questionNumber++;
+                    }
+                    else {
+                      //TODO: 1.ถ้าคำถามหมดแล้ว ให้เริ่มต้นใหม่ โดยกลับไปที่คำถามเดิมและเคลียร์ scoreKeeper ด้วย
+
+                      questionNumber = 0;
+                      scoreKeeper.clear();
+
+                      Score = 0;
+
+                    }
+
+                  });
+                }
               },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.blue
+              ),
+              child: Text(
+                'Restart',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+
+              onPressed: () {
+                setState(() {
+
+                    questionNumber = 0;
+                    scoreKeeper.clear();
+
+                    Score = 0;
+
+
+
+                  });
+                },
             ),
           ),
         ),
@@ -142,4 +276,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
